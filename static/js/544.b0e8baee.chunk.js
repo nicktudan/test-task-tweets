a0b53a91c706e9a3b7ee0b294128d0610a39e20a
nicktudan <107770913+nicktudan@users.noreply.github.com>
2023-04-25 14:21:22 +0000

    Deploying to gh-pages from @ nicktudan/test-task-tweets@ea007fe44e784e058a4cebf75ff5452a20602880 🚀

---
+++ static/js/544.b0e8baee.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[544],{544:function(e,t,r){r.r(t);var c=r(184);t.default=function(){return(0,c.jsx)("div",{children:(0,c.jsx)("h1",{children:"Welcome to Tweet!"})})}}}]);
+//# sourceMappingURL=544.b0e8baee.chunk.js.map
---
+++ static/js/997.d85a988f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[997],{997:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)(a.Fragment,{children:(0,a.jsx)("h2",{children:"Cards Tweets"})})}}}]);
+//# sourceMappingURL=997.d85a988f.chunk.js.map
